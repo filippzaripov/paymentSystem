2036025e3d0dd6dcc3590d55b5c46b038195017f
filippzaripov <philippsbucket@gmail.com>
2018-04-30 17:14:40 +0300

    merge to master

--- src/main/java/com/aci/paymentSystem/repositories/PaymentRepository.java
+++ src/main/java/com/aci/paymentSystem/repositories/PaymentRepository.java
@@ -5,7 +5,6 @@ import com.aci.paymentSystem.model.Customer;
 import com.aci.paymentSystem.model.Payment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
@@ -14,11 +13,10 @@ import java.util.List;
  */
 
 @Repository
-@CrossOrigin(origins = "http://localhost:4200")
 public interface PaymentRepository extends CrudRepository<Payment, Integer> {
 
-    public List<Payment> findByCustomerId(int id);
+    public List<Payment> findByCustomerId(int topicId);
 
-    public List<Payment> findByBillerId(int id);
+    public List<Payment> findByBiller(Biller biller);
 
 }
